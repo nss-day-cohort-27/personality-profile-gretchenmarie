c5c763fb2806328488827129d0008f57d03a0c28
gretchenmarie <gretchenmward@gmail.com>
2018-07-26 15:38:45 -0500

    database to the DOM

--- data.js
+++ data.js
@@ -14,16 +14,16 @@ const personality = {
     },
 
     people = {
-        same: "These four people share my personality-Klaus, Nick, Janet, Kayla"
+        same: ""
 
     },
 
     communication = {
-    
-            withsib: "When communicating with my sib I would let them know the lines of communication are open and periodically check in.",
-            withinstructors: "When communicating with instructors I like using slack and having them available in class to help",
-            withTeam: "In tema projects we have used slack and open discussion to communicate. That has been very effective"
-        },
+
+        withsib: "When communicating with my sib I would let them know the lines of communication are open and periodically check in.",
+        withinstructors: "When communicating with instructors I like using slack and having them available in class to help",
+        withTeam: "In tema projects we have used slack and open discussion to communicate. That has been very effective"
+    },
     ]
 }
 
@@ -34,37 +34,35 @@ const personality = {
 
 
 const profile = function (databaseObject, keyName) {
-    const personalityString =JSON.stringify(personality);
-    localStorage.setItem( keyName,personalityString);
+    const personalityString = JSON.stringify(personality);
+    localStorage.setItem(keyName, personalityString);
 }
 
-    //use function name (object name that you are passing through (personality), "key" ( new made up name))
-
-   profile(personality, "personalityKey");
-
-  //  const personalitySection = localStorage.getItem("keyProfile");
-  //  let data = JSON.parse(personalitySection);
+//use function name (object name that you are passing through (personality), "key" ( new made up name))
 
 
+profile(personality, "personalityKey");
 
-  //  for (let personalityList in data ){
-  //      for (let i = 0; i < data[personalityList].length; i++) {
-  //          const currentObject = data[personalityList][i];
-  //          console.log (currentObject);
-            // tagging the list and the elements that go inside are list items
-   //         const ultag = document.createElement("li")
-            //get the element-create an id in the ul tage the child is the li 
-            //named ultag then go into the HTML (innerHTML) then the new constant called currentObject
-   //         document.getElementById("personalityList").appendChild(ultag).innerHTML =
-   //          `${currentObject.type}, ${currentObject.yesAgree}, ${currentObject.reason},
-   //          ${currentObject.notagree}, ${currentObject.reason2}, ${currentObject.same}
-    //         ${currentObject.withsib}, ${currentObject.withinstructors},${currentObject.withTeam}`;
+ const personalitySection = localStorage.getItem("personalityKey");
+ let data = JSON.parse(personalitySection);
 
 
 
+for (let personalityList in data) {
+    for (let i = 0; i < data[personalityList].length; i++) {
+        const currentObject = data[personalityList][i];
+        console.log(currentObject);
+        // tagging the list and the elements that go inside are list items
+                const ultag = document.createElement("li")
+        //get the element-create an id in the ul tag the child is the li 
+        //named ultag then go into the HTML (innerHTML) then the new constant called currentObject
+        document.getElementById("personalityList").appendChild(ultag).innerHTML =
+            `${currentObject.type}, ${currentObject.yesAgree}, ${currentObject.reason},
+             ${currentObject.notagree}, ${currentObject.reason2}, ${currentObject.same},
+             ${currentObject.withsib}, ${currentObject.withinstructors},${currentObject.withTeam}`;
 
-
-
+    };
+}
 
 
 
@@ -85,3 +83,9 @@ const profile = function (databaseObject, keyName) {
 
 
 
+
+
+
+
+
+    
